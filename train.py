86d6cb4d52ba712451a63889c554eff214397fcd
Megastrik3 <megastrik3@gmail.com>
2025-06-19 08:37:46 -0400

    improvements

--- train.py
+++ train.py
@@ -1,21 +1,17 @@
 import shutil
 import gpu_verify
 from ultralytics import YOLO
+import subprocess
 
 
 def main():
     print("Starting YOLO Training...")
-    model = YOLO("yolo11n.pt")  # Load a COCO-pretrained YOLO11n model
-    results = model.train(data="datasets/bus-aps/data.yaml", epochs=100, imgsz=640)
+    model = YOLO("yolo11n-seg.pt")  # Load a COCO-pretrained YOLO11n model
+    results = model.train(data="datasets/bus-aps/data.yaml", epochs=1, imgsz=640)
     print("Training completed.")
     print(results)
-    if gpu_verify.check_gpu():
-        model_format = "engine"
-    else:
-        model_format = "onnx"
-
-    output = model.export(format=model_format)
-    shutil.copy(output, f"./busaps.{model_format}")
+    import model_export
+    model_export.export_model(model)
 
 
 if __name__ == '__main__':
