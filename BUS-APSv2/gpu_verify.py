1de92c81b44efd15afce3d2bd6379e66985ce55f
Megastrik3 <53279958+Megastrik3@users.noreply.github.com>
2025-06-27 09:07:47 -0400

    removed unused files

--- BUS-APSv2/gpu_verify.py
+++
@@ -1,9 +0,0 @@
-import torch
-# Check if GPU is available
-def check_gpu():
-    if torch.cuda.is_available():
-        print("GPU Name:", torch.cuda.get_device_name(0))
-        return True
-    else:
-        print("No GPU available.")
-        return False
