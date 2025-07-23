717558d7f39e56f4f13482eed7038c662d4a6def
Megastrik3 <53279958+Megastrik3@users.noreply.github.com>
2025-07-22 21:54:40 -0400

    training updates

--- train.py
+++ train.py
@@ -27,8 +27,8 @@ export_formats = {
 def main():
     print("Starting YOLO Training...")
     model = YOLO("yolo11n.pt")  # Load a COCO-pretrained YOLO11n model
-    model.train(data="datasets/bus-aps/data.yaml", epochs=450, imgsz=640, batch=-1, device=0, patience=50, save=True, save_period=10)
-    #, hsv_h=0.25, hsv_s=0.5, hsv_v=0.5, translate=0.25, scale=0.3, fliplr=0.2
+    model.train(data="datasets/bus-aps/data.yaml", epochs=450, imgsz=640, batch=-1, device=0, patience=50, save=True, save_period=10, hsv_h=0.25, hsv_s=0.5, hsv_v=0.5, translate=0.25, scale=0.3, fliplr=0.2)
+    #
     print("Training completed.")
     try:
         version_code = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
@@ -71,7 +71,7 @@ def export_model(model_name='best.pt'):
             print("GPU not detected. Cannot export to engine format.")
             raise Exception("GPU not detected. Cannot export to engine format.")
         model = YOLO(model_name)  # Load the model
-        output = model.export(format=export_formats[export_format], name=model_name)
+        output = model.export(format=export_formats[export_format], name=model_name, data="datasets/bus-aps/data.yaml")
         print(f"Model exported successfully in {export_formats[export_format]} format.")
         print(output)
         print("Exporting completed successfully")
