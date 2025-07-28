5bfd973cc35e493a80b96128916a29c41dd820fa
Megastrik3 <53279958+Megastrik3@users.noreply.github.com>
2025-07-28 11:36:04 -0400

    removed unecessary files

--- benchmark.py
+++
@@ -1,9 +0,0 @@
-from ultralytics.utils.benchmarks import benchmark
-import model_metrics
-
-
-
-if __name__ == "__main__":
-    selected_model = model_metrics.getModels()
-    # Benchmark on GPU
-    benchmark(model=selected_model, data="./datasets/bus-aps/data.yaml", imgsz=640, device=0, verbose=True)
